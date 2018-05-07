da709136f76438910e968c3cda8516e58cdccfa6
Tammo0987 <tammo.steffens@googlemail.com>
2018-05-07 21:33:33 +0200

    Improved file network utils
    Signed-off-by: Tammo0987 <tammo.steffens@googlemail.com>

--- Open-Network/src/main/java/de/tammo/cloud/network/utils/PacketFile.java
+++ Open-Network/src/main/java/de/tammo/cloud/network/utils/PacketFile.java
@@ -4,12 +4,15 @@
 
 package de.tammo.cloud.network.utils;
 
+import lombok.Getter;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
 public class PacketFile {
 
+    @Getter
     private final byte[] bytes;
 
     public PacketFile(final byte[] bytes) {
