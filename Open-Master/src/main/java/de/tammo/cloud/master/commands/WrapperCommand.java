d73633429c3f992b757866202364d6ae0db0ab2d
Tammo0987 <tammo.steffens@googlemail.com>
2018-05-07 20:30:22 +0200

    Added wrapper command
    Signed-off-by: Tammo0987 <tammo.steffens@googlemail.com>

---
+++ Open-Master/src/main/java/de/tammo/cloud/master/commands/WrapperCommand.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2018. File created by Tammo
+ */
+
+package de.tammo.cloud.master.commands;
+
+import de.tammo.cloud.core.command.Command;
+import de.tammo.cloud.master.Master;
+import de.tammo.cloud.master.network.wrapper.Wrapper;
+import de.tammo.cloud.master.network.wrapper.WrapperMeta;
+
+import java.util.UUID;
+
+@Command.CommandInfo(name = "wrapper", aliases = {"w"})
+public class WrapperCommand implements Command {
+
+    public void execute(final String[] args) {
+        if (args.length == 1) {
+            if (args[0].equalsIgnoreCase("list")) {
+                Master.getMaster().getLogger().info("<-- Wrapper -->");
+                Master.getMaster().getNetworkHandler().getWrappers().forEach(wrapper -> Master.getMaster().getLogger().info("Wrapper on host " + wrapper.getWrapperMeta().getHost() + " with unique id " + wrapper.getWrapperMeta().getUuid().toString() + (wrapper.isConnected() ? " is connected" : " is not connected")));
+                Master.getMaster().getLogger().info("");
+            } else {
+                this.printSyntax();
+            }
+        } else if (args.length == 2) {
+            if (args[0].equalsIgnoreCase("info")) {
+                final Wrapper wrapper = Master.getMaster().getNetworkHandler().getWrapperByHost(args[1]);
+                if (wrapper == null) {
+                    Master.getMaster().getLogger().warn("Wrapper not available!");
+                } else {
+                    Master.getMaster().getLogger().info("Wrapper with unique id " + wrapper.getWrapperMeta().getUuid().toString() + " is " + (wrapper.isConnected() ? "connected" : "not connected"));
+                }
+            } else if (args[0].equalsIgnoreCase("create")) {
+                if (Master.getMaster().getNetworkHandler().getWrapperByHost(args[1]) != null) {
+                    Master.getMaster().getLogger().warn("Wrapper already exists!");
+                    return;
+                }
+
+                final Wrapper wrapper = new Wrapper(new WrapperMeta(UUID.randomUUID(), args[1]));
+                Master.getMaster().getNetworkHandler().addWrapper(wrapper);
+                Master.getMaster().getLogger().info("Added wrapper on host " + wrapper.getWrapperMeta().getHost() + " with unique id " + wrapper.getWrapperMeta().getUuid().toString());
+            } else if (args[0].equalsIgnoreCase("delete")) {
+                final Wrapper wrapper = Master.getMaster().getNetworkHandler().getWrapperByHost(args[1]);
+                if (wrapper == null) {
+                    Master.getMaster().getLogger().warn("Wrapper is not created yet!");
+                    return;
+                }
+                Master.getMaster().getNetworkHandler().removeWrapper(wrapper);
+                Master.getMaster().getLogger().info("Removed wrapper with host " + wrapper.getWrapperMeta().getHost() + "!");
+            } else {
+                this.printSyntax();
+            }
+        } else {
+            this.printSyntax();
+        }
+    }
+
+    private void printSyntax() {
+        Master.getMaster().getLogger().info("wrapper list");
+        Master.getMaster().getLogger().info("wrapper info <host>");
+        Master.getMaster().getLogger().info("wrapper create <host>");
+        Master.getMaster().getLogger().info("wrapper delete <host>");
+    }
+
+}
