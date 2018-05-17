693ce7087e32565e91a081f92d9188c6806719a7
Tammo0987 <tammo.steffens@googlemail.com>
2018-05-17 21:38:39 +0200

    Remove empty and unused constructor
    Signed-off-by: Tammo0987 <tammo.steffens@googlemail.com>

--- Open-Core/src/main/java/de/tammo/cloud/core/exceptions/FileDownloadException.java
+++ Open-Core/src/main/java/de/tammo/cloud/core/exceptions/FileDownloadException.java
@@ -6,8 +6,6 @@ package de.tammo.cloud.core.exceptions;
 
 public class FileDownloadException extends Exception {
 
-    public FileDownloadException() {}
-
     public FileDownloadException(final String message) {
         super(message);
     }
